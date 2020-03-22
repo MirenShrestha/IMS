3deffc106b33f200ae840849b842dbb332263ea7
Miren Shrestha <shresthamiren@gmail.com>
2020-03-22 23:36:08 +0545

    USER VIEW COMPLETE.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using InventoryManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+namespace InventoryManagementSystem.Controllers
+{
+    public class UserController : Controller
+    {
+        IMSEntities1 db = new IMSEntities1();
+        // GET: User
+        public ActionResult Index()
+        {
+            var dbUser = db.tbl_User.Include(m => m.tbl_Role).ToList();
+            var vmUser = (from u in dbUser
+
+                          select new UserViewModel
+                          {
+                              Id = u.Id,
+                              Username = u.Username,
+                              Firstname = u.Firstname,
+                              Lastname = u.Lastname,
+                              Email = u.Email,
+                              ContactNo = u.ContactNo,
+                              Address = u.Address,
+                              Status = u.Status,
+                              Role = u.tbl_Role.Role
+                          }).ToList();
+            return View(vmUser);
+        }
+
+
+
+
+    }
+}
---
+++ Models/UserViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InventoryManagementSystem.Models
+{
+    public class UserViewModel
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public long ContactNo { get; set; }
+        public string Address { get; set; }
+        public string Status { get; set; }
+        public int RoleId { get; set; }
+        public string Role { get; set; }
+        public List<System.Web.Mvc.SelectListItem> RoleList { get; set; }
+
+
+    }
+}
